795d3a25f8b17544ecc5b8aee90a683cb01cf24c
abdallah-alabed <abd.alabed97@gmail.com>
2021-09-20 20:17:50 +0300

    done

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
 "use strict";
 
 require("dotenv").config();
-
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
 app.use(cors());
-
+app.use(express.json());
 const PORT = process.env.PORT 
 const mongoose=require("mongoose");
 const MONGO_SERVER=process.env.MONGO_SERVER
-
-const { bookController } = require("./controllers/Book.controller");
-
+const { bookController,createBookController,deleteBookController } = require("./controllers/Book.controller");
 mongoose.connect(`${MONGO_SERVER}/books`,{useNewUrlParser: true, useUnifiedTopology: true});
 
 
+
+
+
 app.get('/books',bookController)
+app.post('/create',createBookController)
+app.delete('/delete/:id',deleteBookController);
+
+
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
 
